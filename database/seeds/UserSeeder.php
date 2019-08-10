e1c20034194b8e00ce625a36677cede1b2a78e7d
febrianto99 <febrianmarkoto@gmail.com>
2019-08-10 13:01:05 +0700

    first ujikom

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //membuat role
+        $adminRole = new Role();
+        $adminRole->name = "superadmin";
+        $adminRole->display_name = "admin";
+        $adminRole->save();
+
+        $admin = new User ();
+        $admin->name = "Febrianto";
+        $admin->email = "admin@gmail.com";
+        $admin->password =bcrypt ('rahasia');
+        $admin->save();
+        $admin->attachRole($adminRole);
+    }
+}
