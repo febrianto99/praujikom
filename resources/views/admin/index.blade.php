01a84b2833b7c5d6cdb058e1eadc345e4e5fdaf2
febrianto99 <febrianmarkoto@gmail.com>
2019-08-10 12:38:18 +0700

    first praujikom

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                   
+                    <b> {{ Auth::user()->name }}</b> selamat datang
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Auth::routes(['register' => true]);
 
+Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:superadmin']], function(){
     Route::get('/', function() {
         return 'halo';
